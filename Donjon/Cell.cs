aae96886f311394519d4d0529855000956f847ef
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2018-12-06 12:05:19 +0100

    Add project files.

---
+++ Donjon/Cell.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Donjon
+{
+    internal class Cell
+    {
+        public Creature Creature { get; set; }
+        public List<Item> Items { get; set; } = new List<Item>();
+    }
+}
---
+++ Donjon/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Donjon
+{
+    internal class Game
+    {
+        Map map;
+        Hero hero;
+
+        public Game()
+        {
+            map = new Map(width: 15, height: 15);
+            hero = new Hero();
+        }
+        
+        internal void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
